17e8b9d20ed41cf1f9bd1178368007afeecf0c74
roni3103 <roni.colling1@gmail.com>
2016-08-26 12:16:29 +0100

    Add files via upload
    From our training project - to work on this to include Express and Angular

---
+++ main.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(__dirname + '/dollproject'));
+
+//when a get request is made on the url /shop then the response sends the appropriate file
+app.get('/shop', function (req, res) {
+   res.sendFile(path.join(__dirname + '/dollproject/products.html'));
+});
+
+/*app.get('/:name', function(req,res) {
+res.send('Hello ' + req.params.name);
+});*/
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
